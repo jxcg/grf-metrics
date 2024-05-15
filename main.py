8f232045e8a05720056312bbb9c664157e97184f
jxcg <154761377+jxcg@users.noreply.github.com>
2024-05-15 23:03:38 +0100

    Modified main.py, will add branches later

--- main.py
+++ main.py
@@ -1,4 +1,21 @@
+import prometheus_client
+from scapy.all import *
+
+class NetworkClient:
+    def __init__(self):
+        pass
+
+    def packet_handler(self, packet):
+        print(packet.summary())
+
 
 class PromClient:
     def __init__(self):
         pass
+
+
+if __name__ == "__main__":
+    print("app started")
+    pc = PromClient()
+    nc = NetworkClient()
+    sniff(prn=nc.packet_handler, count=10)
